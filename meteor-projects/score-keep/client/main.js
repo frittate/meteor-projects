797480709e8b9d1479e54b86d180b6163c21f037
DESKTOP-FPH99T9\Basti <acedia247@gmail.com>
2018-01-16 16:32:42 +0100

    rendered dynamic array (3.14)

--- meteor-projects/score-keep/client/main.js
+++ meteor-projects/score-keep/client/main.js
@@ -2,8 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [{
+    _id: '1',
+    name: 'Basti',
+    score: 50,
+}, {
+    _id: '2',
+    name: 'Kat',
+    score: 99,
+}, {
+    _id: '3',
+    name: 'Erik',
+    score: 12,
+}];
+
+const renderPlayers = function(playersList){
+    return playersList.map(function(player){
+        return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+    });
+};
+
 Meteor.startup(function(){
+    let headline = 'Score Keep';
     let name = 'Sebastian Martin';
-    let jsx = <p>Welcome {name}</p>;
+    let jsx = (
+    <div>
+        <h1>{headline}</h1>
+        {renderPlayers(players)} 
+    </div>
+    );
     ReactDOM.render(jsx,document.getElementById('app'));
 });
