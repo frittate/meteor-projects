624b25cfb8156f8403267e688619cc681098084e
DESKTOP-FPH99T9\Basti <acedia247@gmail.com>
2018-01-16 16:55:24 +0100

    Build a players DB in mongo

--- meteor-projects/score-keep/imports/utils.js
+++
@@ -1,8 +0,0 @@
-console.log('Log from /imports/utils.js');
-
-export let greetUser = function() {
-    return 'Hello there!';
-};
-
-export let name = 'Basti';
-export default 'Default val';
--- meteor-projects/score-keep/server/main.js
+++ meteor-projects/score-keep/server/main.js
@@ -1,8 +1,10 @@
-//import './../imports/utils';
+import {Meteor} from 'meteor/meteor';
+import {Players} from './../imports/api/players';
 
-import {greetUser} from './../imports/utils';
-import add from './../imports/math';
-
-console.log('Log from /server/main.js');
-console.log(greetUser());
-console.log(add(11,12));
+Meteor.startup(function(){
+    Players.insert({
+        name: 'Kat',
+        score: 83,
+    });
+    console.log(Players.find().fetch());
+});
